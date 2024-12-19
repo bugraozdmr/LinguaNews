330ce8bed37560b74525f5327b3e55c30989d40c
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2024-12-19 12:35:04 +0300

    fix

--- frontend/api/client.js
+++ frontend/api/client.js
@@ -2,7 +2,8 @@ import { create } from "apisauce";
 
 const api = create({
   // todo try localhost instead
-  baseURL: "http://192.168.1.70:6009",
+  //baseURL: "http://192.168.1.70:6009",
+  baseURL: "http://localhost",
 });
 
 export default api;
